9dd62beb4c3849b181b6aa1affd1e60248dfd20c
mukulkumar10 <74357770+mukulkumar10@users.noreply.github.com>
2021-06-25 14:04:42 +0530

    Add files via upload

---
+++ WeirdAlgorithm.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n>1)
+    {
+        if(n&1){ //condition to check odd number
+            n = 3*n+1;
+        }
+        else{
+            n >>= 1; // n = n/2;
+        }
+        cout<<n<<" ";
+    }
+    return 0;
+}
